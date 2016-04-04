7a9d501af892ab4bbd2f7f2436c43de0bc3c32aa
udaysagar2177 <udaysagar.2177@gmail.com>
2016-04-04 00:41:09 -0700

    Load constants from environment or use default values Add mongodb configuration

---
+++ rest-api/src/main/java/com/rest/config/Constants.java
@@ -0,0 +1,48 @@
+package com.rest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+/**
+ * Created by uday on 3/24/16.
+ */
+
+public class Constants {
+
+    public static final String DATABASE_NAME;
+    public static final String DATABASE_USERNAME;
+    public static final String DATABASE_PASSWORD;
+    public static final String DATABASE_HOSTNAME;
+    public static final int DATABASE_PORT;
+    public static final String DATAPOINT_REST_URL;
+    public static final int NUMBER_OF_TISENSOR_SIMULATIONS;
+    public static final String SIMULATED_TISENSOR_ID_HANDLE =
+            "TISENSOR_SIMULATION_";
+    public static final String SIMULATED_TISENSOR_USER_HANDLE =
+            "TISENSOR_SIMULATION_USER_";
+
+    static {
+        if(System.getenv("environment") == "production") {
+            DATABASE_NAME      = System.getenv("DATABASE_NAME");
+            DATABASE_USERNAME  = System.getenv("DATABASE_USERNAME");
+            DATABASE_PASSWORD  = System.getenv("DATABASE_PASSWORD");
+            DATABASE_HOSTNAME  = System.getenv("DATABASE_HOSTNAME");
+            DATABASE_PORT      = Integer.parseInt(System.getenv(
+                                                        "DATABASE_PORT"));
+            DATAPOINT_REST_URL = System.getenv("DATAPOINT_REST_URL");
+            NUMBER_OF_TISENSOR_SIMULATIONS = Integer.parseInt(System.getenv(
+                    "NUMBER_OF_TISENSOR_SIMULATIONS"));
+        }else {
+            DATABASE_NAME      = "mydb";
+            DATABASE_USERNAME  = "";
+            DATABASE_PASSWORD  = "";
+            DATABASE_HOSTNAME  = "192.168.10.2";
+            DATABASE_PORT      = 27017;
+            DATAPOINT_REST_URL = "http://localhost:8080/datapoint";
+            NUMBER_OF_TISENSOR_SIMULATIONS = 3;
+
+        }
+    }
+}
---
+++ rest-api/src/main/java/com/rest/config/SpringMongoConfig.java
@@ -0,0 +1,43 @@
+package com.rest.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.authentication.UserCredentials;
+
+import com.mongodb.MongoClient;
+
+@Configuration
+public class SpringMongoConfig {
+    private static Logger logger = LoggerFactory.getLogger(
+            SpringMongoConfig.class);
+
+    public @Bean
+    MongoDbFactory mongoDbFactory() throws Exception {
+        logger.debug("Connecting to MongoDB on"+
+                Constants.DATABASE_HOSTNAME+":"+Constants.DATABASE_PORT);
+        logger.debug("Using Database"+Constants.DATABASE_NAME);
+        logger.debug("with Username: "+ Constants.DATABASE_USERNAME+" and " +
+                "password: "+Constants.DATABASE_PASSWORD);
+
+        return new SimpleMongoDbFactory(
+                new MongoClient(
+                        Constants.DATABASE_HOSTNAME,
+                        Constants.DATABASE_PORT
+                ),
+                Constants.DATABASE_NAME,
+                new UserCredentials(
+                        Constants.DATABASE_USERNAME,
+                        Constants.DATABASE_PASSWORD
+                ));
+    }
+
+    public @Bean
+    MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongoDbFactory());
+    }
+}
